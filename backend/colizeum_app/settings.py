7f9fa5863ac723ddef06e13ea0d2fe24658cf450
Mikhail <mikhail.delyanchenko@gmail.com>
2026-08-30 21:14:44 +0300

    changes

--- backend/colizeum_app/settings.py
+++ backend/colizeum_app/settings.py
@@ -7,13 +7,14 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = True
 
-CORS_ALLOW_ALL_ORIGINS = True if DEBUG else False
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
+CORS_ALLOW_ALL_ORIGINS = os.getenv('CORS_ALLOW_ALL_ORIGINS', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '[]').split()
 
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '[]').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
